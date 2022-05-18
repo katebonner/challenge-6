ae7e7c9cf6ab36bbbdc40e36eab2ec2cd4d3ed25
Kate Bonner <katebonner277@gmail.com>
2022-05-18 11:12:33 -0400

    javascript for 5 day forcast

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,11 @@
 var key = "0d36cf1eac97c7615ce2fe5b1d9f0b12";
 
 // GET DATE
-function getTheDate() {
+function getTheDate(day) {
     var currentdate = new Date(); 
+    var dayDate = parseInt(day);
     var datetime = (currentdate.getMonth()+1) + "/"
-                    + currentdate.getDate() + "/"
+                    + (currentdate.getDate() + dayDate) + "/"
                     + currentdate.getFullYear();
     return datetime
     }
@@ -27,7 +28,7 @@ var displayCurrentWeather = function(data) {
     cityHeading.textContent = locallyStoredCity;
     var currentDate = document.createElement("h2");
     currentDate.setAttribute("class", "current-date");
-    currentDate.textContent = getTheDate();
+    currentDate.textContent = getTheDate(0);
     var weatherIcon = document.createElement("img");
     weatherIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png");
     var currentWeatherList = document.createElement("div");
@@ -57,6 +58,38 @@ var displayCurrentWeather = function(data) {
     currentDiv.appendChild(cardDiv);
 }
 
+// DISPLAY FORCAST
+var displayForcast = function(data, i) {
+    console.log("data: " + data);
+    console.log("index: " + i);
+    //CREATE DIVS
+    var forcastCardDiv = document.createElement("div");
+    forcastCardDiv.setAttribute("class", "forcast-weather");
+    var forcastDate = document.createElement("h2")
+    forcastDate.setAttribute("class", "forcast-date");
+    forcastDate.textContent = getTheDate(i);
+    var forcastIcon = document.createElement("img");
+    forcastIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
+    var forcastTemp = document.createElement("p");
+    forcastTemp.setAttribute("class", "forcast-temp");
+    forcastTemp.textContent = "Temperature: " + data.temp.day;
+    var forcastWindSpeed = document.createElement("p");
+    forcastWindSpeed.setAttribute("class", "forcast-wind-spped")
+    forcastWindSpeed.textContent = "Wind Speed: " + data.wind_speed;
+    var forcastHumidity = document.createElement("p");
+    forcastHumidity.setAttribute("class", "forcast-humidity");
+    forcastHumidity.textContent = "Humidity: " + data.humidity;
+
+    //APPEND DIVS
+    forcastCardDiv.appendChild(forcastDate);
+    forcastCardDiv.appendChild(forcastIcon);
+    forcastCardDiv.appendChild(forcastTemp);
+    forcastCardDiv.appendChild(forcastWindSpeed);
+    forcastCardDiv.appendChild(forcastHumidity);
+    forcastDiv.appendChild(forcastCardDiv)
+    
+}
+
 // GET WEATHER FUNCTION
 var getWeather = function(city) {
     var locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + key;
@@ -64,22 +97,24 @@ var getWeather = function(city) {
     fetch(locationUrl)
         .then(function(response) {
             response.json().then(function(data) {
-                console.log(data);
                 var lat = data[0].lat;
                 var long = data[0].lon;
-                console.log(lat);
-                console.log(long);
                 var units = "metric";
                 var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=" + units + "&appid=" + key;
                 fetch(weatherUrl).then(function(response){
                     response.json().then (function(data) {
+                        console.log(data);
+                        // DISPLAY CURRENT WEATHER
                         displayCurrentWeather(data);
-                        // DISPLAY 5 DAY FORCAST (DATA)
-                        // UTILIZE FOR LOOP HERE 
+                        
+                        // DISPLAY 5 DAY FORCAST
+                        for (i = 1 ; i < 6 ; i++){
+                            displayForcast(data.daily[i], i);
+                            console.log(data.daily[i]);
+                        }
                     })
                 })
             })
-
         })
         .catch(function(error) {
             console.log('failed to connect to server')
