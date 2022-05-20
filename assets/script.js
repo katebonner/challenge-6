d33bf1a7b7a9ce2317919fc2879a02d3cd815582
Kate Bonner <katebonner277@gmail.com>
2022-05-20 19:48:08 -0400

    updated search history

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,7 @@ var searchDiv = document.querySelector("#search");
 var currentDiv = document.querySelector("#current");
 var forcastDiv = document.querySelector("#forcast");
 
-// DISPLAY SEARCH BAR + RECENT SEARCH HISTORIES
+// DISPLAY SEARCH BAR
 var searchBar = document.createElement("input");
 searchBar.setAttribute("class", "search-bar");
 var searchBtn = document.createElement("Button");
@@ -25,15 +25,43 @@ searchBtn.textContent = "search";
 searchDiv.appendChild(searchBar);
 searchDiv.appendChild(searchBtn);
 
+// GET RECENT SEARCH HISTORY
+function allStorage() {
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+    while ( i-- ) {
+        values.push(localStorage.getItem(keys[i]));
+    }
+    return values;
+}
+
+// DISPLAY RECENT SEARCH HISTORY
+function displaySearch(){
+    var recentSearch = allStorage();
+    for (i = 0 ; i < recentSearch.length ; i++){
+        var recentSearchEl = document.createElement("p");
+        recentSearchEl.setAttribute("class", "search-element");
+        recentSearchEl.textContent = recentSearch[i];
+        searchDiv.appendChild(recentSearchEl);
+    }
+}
+
+// // CLEAR RECENT SEARCH DIVS
+var clearSearch = function() {
+    var oldSearchElement = $(".search-element");
+    oldSearchElement.remove();
+}
+
 // DISPLAY CURRENT WEATHER DATA
-var displayCurrentWeather = function(data) {
+var displayCurrentWeather = function(data, city) {
 
     //CREATE DIVS
     var cardDiv = document.createElement("div");
     cardDiv.setAttribute("class", "current-weather col-12");
     var cityHeading = document.createElement("h2");
     cityHeading.setAttribute("class", "city-heading");
-    var locallyStoredCity = localStorage.getItem("city");
+    var locallyStoredCity = localStorage.getItem(city);
     cityHeading.textContent = locallyStoredCity;
     var currentDate = document.createElement("h3");
     currentDate.setAttribute("class", "current-date");
@@ -55,6 +83,17 @@ var displayCurrentWeather = function(data) {
     var uvIndex = document.createElement("p");
     uvIndex.setAttribute("class", "uvIndex");
     uvIndex.textContent = "UV Index: " + data.current.uvi;
+    
+    // UV INDEX COLOR 
+    if (data.current.uvi < 3.33) {
+        uvIndex.style.backgroundColor = "#93ff07"
+    }
+    else if ((data.current.uvi >= 3.33) && (data.current.uvi < 6.66)){
+        uvIndex.style.backgroundColor = "#ff7f07"
+    }
+    else {
+        uvIndex.style.backgroundColor = "#ff0741"
+    }
 
     //APPEND DIVS
     cardDiv.appendChild(cityHeading);
@@ -70,8 +109,7 @@ var displayCurrentWeather = function(data) {
 
 // DISPLAY FORCAST
 var displayForcast = function(data, i) {
-    console.log("data: " + data);
-    console.log("index: " + i);
+
     //CREATE DIVS
     var forcastCardDiv = document.createElement("div");
     forcastCardDiv.setAttribute("class", "forcast-weather col-md-2 col-6");
@@ -101,7 +139,6 @@ var displayForcast = function(data, i) {
 }
 
 // CLEAR WEATHER
-
 var clearWeather = function() {
     var oldCurrentElement = document.querySelector(".current-weather")
     var oldForcastElement = document.querySelector(".forcast-weather")
@@ -118,7 +155,7 @@ var clearWeather = function() {
 // GET WEATHER FUNCTION
 var getWeather = function(city) {
     var locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + key;
-    localStorage.setItem("city", city);
+    localStorage.setItem(city, city);
     fetch(locationUrl)
         .then(function(response) {
             response.json().then(function(data) {
@@ -128,33 +165,29 @@ var getWeather = function(city) {
                 var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=" + units + "&appid=" + key;
                 fetch(weatherUrl).then(function(response){
                     response.json().then (function(data) {
-                        console.log(data);
+            
                         // DISPLAY CURRENT WEATHER
-                        displayCurrentWeather(data);
-                        
+                        displayCurrentWeather(data, city);
+
                         // DISPLAY 5 DAY FORCAST
                         for (i = 1 ; i < 6 ; i++){
                             displayForcast(data.daily[i], i);
-                            console.log(data.daily[i]);
                         }
                     })
                 })
             })
         })
         .catch(function(error) {
-            console.log('failed to connect to server')
+            alert('failed to connect to server');
         })
 
 }
-
-$(".search-bar").autocomplete({
-    // source: LOCAL STORAGE
-  });
-
+displaySearch();
 $(".search-button").click(function() {
     var searchInput = $(".search-bar").val();
-    localStorage.setItem(searchInput, searchInput);
     clearWeather();
     getWeather(searchInput);
     $(".search-bar").val("");
+    clearSearch();
+    displaySearch();
  });
